ccd344fb4177ccc8cfd02f106b4d2710574a3e94
LionTutor <117615234+LionTutor@users.noreply.github.com>
2022-12-15 22:53:13 -0600

    Add files via upload

---
+++ version6/VendingMachine/src/main/java/com/sg/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.vendingmachine.controller;
+
+import ServiceMoney.Change;
+import ServiceMoney.moneyChange;
+import com.sg.vendingmachine.dao.VendingMachineDao;
+import com.sg.vendingmachine.dao.VendingMachineDaoException;
+import com.sg.vendingmachine.dao.VendingMachineDaoFileImpl;
+import com.sg.vendingmachine.dto.Product;
+import com.sg.vendingmachine.service.InsufficientFundsException;
+import com.sg.vendingmachine.service.NoItemInventoryException;
+import com.sg.vendingmachine.service.VendingMachineServiceLayer;
+import com.sg.vendingmachine.ui.UserIO;
+import com.sg.vendingmachine.ui.UserIOConsoleImpl;
+import com.sg.vendingmachine.ui.VendingMachineView;
+import java.math.BigDecimal;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author lionwife
+ */
+public class VendingMachineController {
+    private VendingMachineView view; 
+            //= new VendingMachineView();
+    private UserIO io = new UserIOConsoleImpl();
+    private VendingMachineDao dao = new VendingMachineDaoFileImpl();
+    private VendingMachineServiceLayer service;
+    
+    public VendingMachineController(VendingMachineServiceLayer service, VendingMachineView view) {
+    this.service = service;
+    this.view = view;
+}
+    
+    public void run() throws VendingMachineDaoException, NoItemInventoryException, InsufficientFundsException {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+           /* io.print("Vending Machine Main Menu: ");
+            io.print("1. TWIX - Price: 100 ");
+            io.print("2. COKE - Price: 50 ");
+            io.print("3. WATER - Price: 30 ");
+            io.print("4. SANDWICH - Price: 150 ");
+            io.print("5. Exit ");
+
+            menuSelection = io.readInt("Please select from the"
+                    + " above choices.", 1, 5);*/
+            menuSelection = getMenuSelection();
+           /* switch (menuSelection) {
+                case 1:
+                    io.print("1");
+                    break;
+                case 2:
+                    io.print("2");
+                    break;
+                case 3:
+                    io.print("3");
+                    break;
+                case 4:
+                    io.print("4");
+                    break;
+                case 5:
+                    io.print("5");
+                    break;
+                case 6:
+                    io.print("6");
+                    break;    
+                case 7:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }*/
+            if(menuSelection==0){
+                keepGoing = false;
+            } else {
+                Product product = dao.getProduct(String.valueOf(menuSelection)); 
+        
+                if (product.getQuantity() <= 0 ) {
+                   io.print("You are purchasing an item that has zero inventory. Please choose other options.");
+                }else{
+                    BigDecimal money=BigDecimal.valueOf(io.readDouble("Please insert money(Calculated in cents): "));               
+                    processOrder(money, product);
+                }
+            }
+            
+        }
+        io.print("GOOD BYE");
+    }
+    
+    private int getMenuSelection() throws VendingMachineDaoException {
+        //return view.printMenuAndGetSelection(dao.getAllProducts());
+        return view.printMenuAndGetSelection(dao.getAllProducts().stream().filter((p) -> p.getQuantity() > 0).collect(Collectors.toList()));
+    }
+    
+    private void processOrder(BigDecimal money, Product product) throws VendingMachineDaoException, NoItemInventoryException, InsufficientFundsException {
+
+        BigDecimal subtract = money.subtract(product.getPrice());        
+        if(subtract.compareTo(BigDecimal.ZERO) < 0){
+          io.print("The money you gave " +money+ " is insufficient.");
+        }else{
+            dao.dispenseProduct(product);
+            Change changes = new Change(subtract.intValue());
+            io.print("You have deposited $"+subtract+" in change.\nConvert your change to the coins are: ");
+            io.print(moneyChange.QUARTERS+"(25Cents): "+changes.getQuarters());
+            io.print(moneyChange.DIMES+"(10Cents): "+ changes.getDimes());
+            io.print(moneyChange.NICKELS+"(5Cents): "+ changes.getNickles());
+            io.print(moneyChange.PENNIES+"(1Cent): "+changes.getPennies()+"\n");
+        }
+    }
+    
+}
---
+++ version6/VendingMachine/src/main/java/com/sg/vendingmachine/dto/Product.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.vendingmachine.dto;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ *
+ * @author lionwife
+ */
+public class Product {
+    private String name;
+    private String id;
+    private BigDecimal price;
+    private int quantity; 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+   /* public void setId(String id) {
+        this.id = id;
+    }*/
+    public Product(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 73 * hash + Objects.hashCode(this.name);
+        hash = 73 * hash + Objects.hashCode(this.id);
+        hash = 73 * hash + Objects.hashCode(this.price);
+        hash = 73 * hash + this.quantity;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (this.quantity != other.quantity) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return Objects.equals(this.price, other.price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name=" + name + ", id=" + id + ", price=" + price + ", quantity=" + quantity + '}';
+    }
+    
+    
+}
